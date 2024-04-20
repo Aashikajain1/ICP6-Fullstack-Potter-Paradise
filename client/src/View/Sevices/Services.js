e0921129d65c53769a9c15d34712067b23c38de0
Aashika Jain <jainaashika1510@gmail.com>
2024-04-20 17:54:53 +0530

    components and view folder added

---
+++ client/src/View/Sevices/Services.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Services() {
+  return (
+    <div className="container text-center border border-primary rounded-3 bg-info text-dark mt-4  d-flex" style={{height:35}} >
+            <h2 style={{color:"red"}}>card</h2>
+            <span><h3>hello</h3></span>       
+        </div>
+  )
+}
+export default Services;
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js"
+import Services from "./View/Sevices/Services"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
   <h1 class="text-warning">HELLOOO</h1>
+  <Services/>
+  </div>
 );
 
